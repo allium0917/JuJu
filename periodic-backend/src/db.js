75f1ad6c18b98e8f202b098690c461843680f789
allium0917 <sistus0917@gmail.com>
2025-12-07 21:03:44 +0900

    db connect modified

--- periodic-backend/src/db.js
+++ periodic-backend/src/db.js
@@ -1,12 +1,13 @@
 import pkg from 'pg';
+import 'dotenv/config';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'myuser',
-  host: 'localhost',
-  database: 'science',
-  password: '0917',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 async function testConnection() {
